a6e3e6d964babe448cb368755267fc87956071b0
Betty C <bettychchow@gmail.com>
2017-04-15 13:11:44 -0700

    Add files via upload

--- tree.js
+++ tree.js
@@ -8,21 +8,15 @@ class Node {
 const sumNode = (node) => {
   let sum = 0;
   if (node) { sum += node.value}
-  console.log(sum);
 
   let remainder = node.children;
 
 while(remainder[0]) {
-  console.log('Kona', remainder);
   sum += remainder[0].value;
-  console.log('kkk', sum);
   if (remainder[0].children !== undefined) {
 remainder = remainder.concat(remainder[0].children)
 }
     remainder.splice(0,1);
-    console.log(remainder);
-
-    console.log('hi', sum);
 }
 
 return sum;
