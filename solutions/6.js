6065643edea77747ac3954c5886d6aab337916cc
Betty C <bettychchow@gmail.com>
2017-04-15 13:09:08 -0700

    Add files via upload

--- solutions/6.js
+++ solutions/6.js
@@ -1,11 +1,34 @@
 class Node {
-  constructor(v) {
+  constructor(v, children) {
     this.value = v;
-    this.children = [];
+    this.children = children;
   }
 }
 
-module.exports = {
-  node: Node,
+const sumNode = (node) => {
+  let sum = 0;
+  if (node) { sum += node.value}
+  console.log(sum);
+
+  let remainder = node.children;
+
+while(remainder[0]) {
+  console.log('Kona', remainder);
+  sum += remainder[0].value;
+  console.log('kkk', sum);
+  if (remainder[0].children !== undefined) {
+remainder = remainder.concat(remainder[0].children)
+}
+    remainder.splice(0,1);
+    console.log(remainder);
+
+    console.log('hi', sum);
+}
+
+return sum;
 };
 
+module.exports = {
+  node: Node,
+  sumNode: sumNode
+};
