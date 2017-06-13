f7ed06abcbbe10aaecfdc14dcdbee34196d95ac0
Betty Chow <bettychchow@gmail.com>
2017-06-13 16:03:39 -0700

    error on test fixed

--- test/5.js
+++ test/5.js
@@ -54,16 +54,16 @@ describe('sort arrays', () => {
   it('solution1 - should generate numbers in ascending order', () => {
     const input = [3,1,2,4];
     const output = [1,2,3,4];
-    expect(solution(input)).eql(output);
+    expect(solution1(input)).eql(output);
   });
   it('solution1 - should generate numbers in ascending order', () => {
     const input = ['betty', 'amy', 'grace', 'naomi', 'rose'];
     const output = ['amy', 'betty', 'grace', 'naomi', 'rose'];
-    expect(solution(input)).eql(output);
+    expect(solution1(input)).eql(output);
   });
   it('solution1 - should generate numbers in ascending order', () => {
     const input = ['Thomas', 'Pikachu', 'Superman', 'Batman', 'Pingu' ];
     const output = ['Batman', 'Pikachu', 'Pingu', 'Superman', 'Thomas'];
-    expect(solution(input)).eql(output);
+    expect(solution1(input)).eql(output);
   });
 });
